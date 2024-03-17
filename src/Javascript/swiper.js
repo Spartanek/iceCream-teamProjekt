66fd3a8f6513ee084b70621aa4031a068d327445
Daniil191245 <131523169+Daniil191245@users.noreply.github.com>
2024-03-17 11:57:23 +0200

    start

--- src/Javascript/swiper.js
+++ src/Javascript/swiper.js
@@ -1,21 +1,13 @@
 const swiper = new Swiper('.swiper', {
-  // Optional parameters
-  direction: 'vertical',
-  loop: true,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
-
-  // Navigation arrows
+  cssMode: true,
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
   },
-
-  // And if we need scrollbar
-  scrollbar: {
-    el: '.swiper-scrollbar',
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
   },
+  mousewheel: true,
+  keyboard: true,
 });
