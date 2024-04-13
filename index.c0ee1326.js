7e957d0bb1ab835ad4d7805b028551b69e798646
Spartanek <123250645+Spartanek@users.noreply.github.com>
2024-04-13 08:46:26 +0000

    Deploying to gh-pages from @ Spartanek/iceCream-teamProjekt@d8277b6ec374b25f9d86c7db9c03a308c1a47379 🚀

---
+++ index.c0ee1326.js
@@ -0,0 +1,2 @@
+var swiper=new Swiper(".swiper",{cssMode:!0,navigation:{nextEl:".swiper-button-next",prevEl:".swiper-button-prev"},pagination:{el:".swiper-pagination",clickable:!0},mousewheel:!0,keyboard:!0});
+//# sourceMappingURL=index.c0ee1326.js.map
