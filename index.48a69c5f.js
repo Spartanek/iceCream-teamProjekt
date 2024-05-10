ceaece0d380255fc081c6e42fc7cfeb96ac823c2
Spartanek <123250645+Spartanek@users.noreply.github.com>
2024-05-10 10:24:15 +0000

    Deploying to gh-pages from @ Spartanek/iceCream-teamProjekt@b6b0eb84f0e8cefcd80299df75cf0fdf69fff514 🚀

--- index.48a69c5f.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openMenuBtn:document.querySelector(".mob-menu__button--open"),closeMenuBtn:document.querySelector(".mob-menu__button--close"),menu:document.querySelector(".mob-menu")};function n(){e.menu.classList.toggle("visually-hidden")}e.openMenuBtn.addEventListener("click",n),e.closeMenuBtn.addEventListener("click",n)}();
-//# sourceMappingURL=index.48a69c5f.js.map
